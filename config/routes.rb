91901e41d0394b5b7c94e6972d76111d6f4e044b
Szymon Komorowski <skomorowski@sigma.ug.edu.pl>
2014-04-03 07:02:20 +0200

    added routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 MyGists::Application.routes.draw do
   resources :gists
+  root 'gists#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
